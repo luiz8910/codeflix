412c5f85555c3f84946b5ebdcfad9feb77807feb
Luiz Sanches <luiz.sanches8910@gmail.com>
2024-01-02 21:29:48 -0300

    Datagrid da listagem

--- src/features/categories/ListCategory.tsx
+++ src/features/categories/ListCategory.tsx
@@ -1,13 +1,51 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { useAppSelector } from "../../app/hooks";
 import { selectCategories } from "./categorySlice";
+import { Link } from "react-router-dom";
+import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
 
 export const CategoryList = () => {
+  const categories = useAppSelector(selectCategories);
+
+  const rows: GridRowsProp = categories.map((category) => ({
+    id: category.id,
+    name: category.name,
+    description: category.description,
+  }));
+
+  /* const rows: GridRowsProp = [
+    { id: 1, col1: "Hello", col2: "World" },
+    { id: 2, col1: "DataGridPro", col2: "is Awesome" },
+    { id: 3, col1: "MUI", col2: "is Amazing" },
+  ]; */
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 150 },
+    { field: "name", headerName: "Name", width: 150 },
+    { field: "description", headerName: "Description", width: 150 },
+  ];
+
   return (
-    <Box>
-      <Typography variant="h3" component="h1">
-        CategoryList Page
-      </Typography>
+    <Box maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Box display="flex" justifyContent="flex-end">
+        <Button
+          variant="contained"
+          color="secondary"
+          component={Link}
+          to="/category/create"
+          style={{ marginBottom: "1rem" }}
+        >
+          New Category
+        </Button>
+      </Box>
+
+      {/* {categories.map((category) => (
+        <Typography key={category.id}>{category.name}</Typography>
+      ))} */}
+
+      <div style={{ height: 300, width: "100%" }}>
+        <DataGrid rows={rows} columns={columns} />
+      </div>
     </Box>
   );
 };
